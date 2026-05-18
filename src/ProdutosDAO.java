cf0e99a45b694b889770157522c65ceb5aaeadee
Enzo <enzofernandes09m@gmail.com>
2026-05-18 18:48:36 -0300

    Botao de salvar cadastrando produto no BD

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,11 +23,18 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+       try {
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
+    }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
